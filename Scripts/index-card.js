643d718d2146267e145d6ee44316af57704a8bae
njisuccess660-oss <njisuccess660@gmail.com>
2026-08-28 11:21:51 +0100

    Added checkbox to login for selection, finish customizing my customer dash-board, added cards with javascript in the home page

---
+++ Scripts/index-card.js
@@ -0,0 +1,49 @@
+// Data source: Array of Service Categories
+const servicesData = [
+  {
+    id: 1,
+    title: "Expert Plumbing",
+    description:
+      "Vetted professionals for leak repair, pipe installation, or...",
+    image: "./images/pictures/plumber.jpg",
+  },
+  {
+    id: 2,
+    title: "Licensed Electrical",
+    description: "Skilled electricians for wiring, fixtures, and panel...",
+    image: "./images/pictures/electrical.jpg",
+  },
+  {
+    id: 3,
+    title: "Custom Carpentry",
+    description: "Quality woodwork, framing, and structural repairs by...",
+    image: "./images/pictures/carpenter.jpg",
+  },
+  {
+    id: 4,
+    title: "Appliance Repair",
+    description: "Fast and reliable repair for refrigerators, ovens, ...",
+    image: "./images/pictures/apliance.jpg",
+  },
+];
+
+function renderServices() {
+  const gridContainer = document.getElementById("dataCard");
+  gridContainer.innerHTML = servicesData
+    .map(
+      (service) => `
+        <div class="service-card" data-id="${service.id}">
+            <div class="card-image-wrapper">
+                <img src="${service.image}" alt="${service.title}" class="service-img">
+            </div>
+            <div class="card-content">
+                <h3>${service.title}</h3>
+                <p>${service.description}</p>
+            </div>
+        </div>
+    `,
+    )
+    .join("");
+}
+
+document.addEventListener("DOMContentLoaded", renderServices);
